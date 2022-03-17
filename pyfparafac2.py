f82af54d041e5b941d9b17bcd685fea98dfa8f99
mdarmstr <mdarmstr@ualbeta.ca>
2022-03-17 09:38:01 -0600

    saving my work

--- pyfparafac2.py
+++ pyfparafac2.py
@@ -13,14 +13,21 @@ def pyfparafac2parse(Xk): #This is working; can either open an .npz or an .npy f
     elif Xk.endswith(".npy"):
         Xk = np.load(Xk)
         sz = np.shape(Xk)
-    return Xk, sz
+        return Xk, sz #may have some redundancies, let's worry about that later :)
 
-def pyfparafac2als(Xk,R):
+def pyfparafac2als(Xk, R):
+    sz = np.shape(Xk)
+    Pk = np.zeros((sz[0], R, sz[3]))
+    for kk in range(sz[2]):
+        U, S, V = np.linalg.svd(Xk[:, :, kk], R)
+        Pk[:, :, kk] = U.dot(np.transpose(V))
 
-def pyfparafac2(Xk,R):
-    Xk, sz = pyfparafac2parse(Xk)
 
 
+def pyfparafac2(Xk, R):
+    Xk = pyfparafac2parse(Xk)
+    Bk, A, Dk, Bs, ssr, pvar = pyfparafac2als(Xk,R)
+    return Bk, A, Dk, Bs, ssr, pvar
 
 
 if __name__ == '__main__':
